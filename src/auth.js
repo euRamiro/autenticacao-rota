c08e8ba690b8935778c46ea8b80312acf60ec270
Ramiro da Silva Amorim <Ramiro da Silva Amorim>
2019-12-01 10:00:05 -0400

    autenticação de rota

---
+++ src/auth.js
@@ -0,0 +1,3 @@
+export const isAuthenticated = () => true;
+//aqui ficaria toda a lógica. exemplo: acessar o localstorage buscar informação de token
+//como não existe um backend retorna true e para testar troca para false
